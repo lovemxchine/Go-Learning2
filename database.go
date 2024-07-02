c86704876f1d873d1e2ae340975c7f749bc98dd1
Channarong Chancher <143964932+lovemxchine@users.noreply.github.com>
2024-07-02 21:14:10 +0700

    api + connect database

---
+++ database.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+func createTask(task *CreateTask) error {
+	currentTime := time.Now().Format("2006-01-02 15:04:05")
+
+	// EXEC() คือการรันคำสั่งเฉยๆไม่ต้องการ return ค่าไรมาเช็คแค่ error หรือ ไม่
+	_, err := db.Exec(
+		`INSERT INTO public.tasks(user_id, task_name, created_at , updated_at , status) VALUES ($1, $2, $3, $4, $5);`,
+		task.User_Id, task.Name, currentTime, currentTime, false)
+
+	return err
+}
+
+func getTask(id int) (Task, error) {
+	var t Task
+	row := db.QueryRow(
+		`SELECT id ,user_id,task_name ,created_at ,updated_at ,status FROM tasks WHERE id = $1;`, id,
+	)
+	err := row.Scan(&t.Id, &t.User_Id, &t.Name, &t.CreateTime, &t.UpdateTime, &t.Status)
+
+	if err != nil {
+		return Task{}, err
+	}
+
+	return t, nil
+}
+
+func getTasks() ([]Task, error) {
+	rows, err := db.Query("SELECT id , user_id, task_name , created_at , updated_at ,status FROM tasks ORDER BY id ASC;")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var tasks []Task
+	for rows.Next() {
+		var t Task
+		err := rows.Scan(&t.Id, &t.User_Id, &t.Name, &t.CreateTime, &t.UpdateTime, &t.Status)
+		if err != nil {
+			return nil, err
+		}
+		tasks = append(tasks, t)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return tasks, nil
+}
+
+func updateTask(id int, task *UpdateTask) error {
+	currentTime := time.Now().Format("2006-01-02 15:04:05")
+	// EXEC() คือการรันคำสั่งเฉยๆไม่ต้องการ return ค่าไรมาเช็คแค่ error หรือ ไม่
+	_, err := db.Exec(
+		"UPDATE public.tasks SET  task_name=$1, updated_at=$2 , status=$3 WHERE id = $4;",
+		task.Name,
+		currentTime,
+		task.Status,
+		id,
+	)
+	return err
+}
+
+func deleteTask(id int) error {
+
+	_, err := db.Exec(
+		"DELETE FROM tasks WHERE id = $1",
+		id,
+	)
+
+	return err
+}
---
+++ main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+	_ "github.com/lib/pq"
+)
+
+const (
+	host         = "localhost"
+	port         = 5432
+	databaseName = "postgres"
+	username     = "postgres"
+	password     = "mypassword"
+)
+
+type Task struct {
+	Id         int    `json:"id"`
+	User_Id    int    `json:"user_id"`
+	Name       string `json:"task_name"`
+	CreateTime string `json:"created_at"`
+	UpdateTime string `json:"updated_at"`
+	Status     bool   `json:"status"`
+}
+
+// for create data
+type CreateTask struct {
+	User_Id int    `json:"user_id"`
+	Name    string `json:"task_name"`
+}
+
+// for update data
+type UpdateTask struct {
+	Name   string `json:"task_name"`
+	Status bool   `json:"status"`
+}
+
+var db *sql.DB
+
+func main() {
+	// Connection string
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, username, password, databaseName)
+
+	sdb, err := sql.Open("postgres", psqlInfo)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app := fiber.New()
+
+	// เชื่อมเสร็จโดยไม่มี error ก็ให้ค่าที่เชื่อมผ่านไปเป็นค่า global
+	defer db.Close()
+
+	db = sdb
+
+	// db.Ping() คือคำสั่งเช็คว่าต่อได้จริงไหม
+	err = db.Ping()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Conntection Database Successful รันอันนี้ได้แปลว่าไม่เจอ error
+	print("Conntection Database Successful\n")
+
+	// Test Server
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("hello world")
+	})
+	// <Create> Create data
+	app.Post("/createTask", createTaskHandler)
+	// <Read> Get data params
+	app.Get("/getTask/:id", getTaskHandle)
+	// <Update> Update data
+	app.Put("/updateTask/:id", updateTaskHandler)
+	// <Delete> Delete Data
+	// app.Delete()
+
+	app.Listen(":8080")
+
+	// tasks, err := getTasks()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(tasks)
+
+}
+
+func getTaskHandle(c *fiber.Ctx) error {
+	taskId, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return c.Status(fiber.ErrBadRequest.Code).SendString(err.Error())
+	}
+	task, err := getTask(taskId)
+	if err != nil {
+		return c.Status(fiber.ErrBadRequest.Code).SendString(err.Error())
+	}
+	return c.JSON(task)
+}
+
+func createTaskHandler(c *fiber.Ctx) error {
+	taskData := new(CreateTask)
+	if err := c.BodyParser(taskData); err != nil {
+		return c.Status(fiber.ErrBadRequest.Code).SendString(err.Error())
+	}
+
+	if err := createTask(taskData); err != nil {
+		return c.Status(fiber.ErrBadRequest.Code).SendString(err.Error())
+	}
+	return c.JSON(taskData)
+}
+
+func updateTaskHandler(c *fiber.Ctx) error {
+	updateTaskData := new(UpdateTask)
+	taskId, err := strconv.Atoi(c.Params("id"))
+	if err := c.BodyParser(updateTaskData); err != nil {
+		return c.Status(fiber.ErrBadRequest.Code).SendString(err.Error())
+	}
+	if err != nil {
+		return c.Status(fiber.ErrBadRequest.Code).SendString(err.Error())
+	}
+	err = updateTask(taskId, updateTaskData)
+	if err != nil {
+		return c.Status(fiber.ErrBadRequest.Code).SendString(err.Error())
+	}
+	return c.JSON(updateTaskData)
+}
